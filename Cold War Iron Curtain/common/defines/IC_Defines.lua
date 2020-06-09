7077c38d8d93d00d1d0de7e0880dd02715bae1a8
Star Foth <40373723+starfoth@users.noreply.github.com>
2020-06-09 00:24:11 -0700

    Fixed and Balanced Air Reconnaissance
    Fixed and balanced air reconnaissance
    -Fixed infinite CP cost
    -Balanced air recon properly

--- Cold War Iron Curtain/common/defines/IC_Defines.lua
+++ Cold War Iron Curtain/common/defines/IC_Defines.lua
@@ -19,7 +19,7 @@ NDefines.NAir.COMBAT_STACK_LIMIT = 1 -- The biggest allowed dogfight combination
 NDefines.NAir.COMBAT_MULTIPLANE_CAP = 2  -- How many planes can shoot at each plane on other side
 NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.05 -- Higher value means more shot down planes
 NDefines.NAir.NAVAL_STRIKE_DAMAGE_TO_STR = 2 -- Balancing value to convert damage ( naval_strike_attack * hits ) to Strength reduction.
-NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }
+NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }
 NDefines.NAir.CAS_NIGHT_ATTACK_FACTOR = 0.5 -- Balancing value to convert damage ( naval_strike_attack * hits ) to Strength reduction.
 
 NDefines.NBuildings.AIRBASE_CAPACITY_MULT = 50 -- Each level of airbase building multiplied by this, gives capacity (max operational value). Value is int. 1 for each airplane.
